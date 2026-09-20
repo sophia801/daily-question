86bee7cfc484ca48ef90bd64a98cb9877df555dc
oophia <sguild@college.harvard.edu>
2026-09-19 22:52:49 -0400

    Connect sparKIT to Supabase

--- dist/config.js
+++ dist/config.js
@@ -1,7 +1,5 @@
 window.SIDEQUEST_CONFIG = {
-  supabaseUrl: "",
-  supabaseAnonKey: "",
+  supabaseUrl: "https://ryzqgjwooujysccttkze.supabase.co",
+  supabaseAnonKey: "sb_publishable__rN6t_rbC7YH8g76TKkbPQ_lhwArwnX",
   displayName: "You",
-  demoCircleCode: "ROOMIE",
 };
-
